c83dd70972dfaa9e9186502007f1c577b7519d7c
ISHA SETHIA <isha.sethia@confluxsys.com>
2022-08-03 12:57:38 +0530

    first commit

--- src/main/java/ExampleH2.java
+++ src/main/java/ExampleH2.java
@@ -1,15 +1,37 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class ExampleH2 {
     public static void main(String[] args) {
+        ExampleH2 obj = new ExampleH2();
+
+        Connection connection;
         try{
-            Class.forName("org.h2.Driver").newInstance();
-            Connection connection = DriverManager.getConnection("jdbc:h2");
+            Class.forName("org.h2.Driver");
+            connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test","sa","");
+            obj.select(connection);
         }catch (Exception e){
-            System.out.println("YAHAN ERROR HAI"+e);
+            System.out.println("YAHAN ERROR HAI:= "+e);
+        }/*finally {
+                try {
+                    assert connection != null;
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }*/
+    }
+    public void select(Connection connection){
+        String query = "Select * from human";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                System.out.println("ID:="+id+" | NAME:"+name);
+            }
+        }catch (Exception e){
+            System.out.println("ERROR: "+e);
         }
-
-
     }
 }
